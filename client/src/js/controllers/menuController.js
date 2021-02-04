652c9ac097744493749d0bd68632bd028e23f74d
yg-0103 <rladusrn89@gmail.com>
2021-02-04 10:04:45 +0900

    feat: merge

--- client/src/js/controllers/menuController.js
+++ client/src/js/controllers/menuController.js
@@ -1,15 +1,20 @@
 import model from '../model/model';
 import menuRender from '../views/menu-render';
 import menuModalRender from '../views/modal-render';
+// //State
+// let start = 0;
+// let end = 8;
 
 // DOM
 const $menuList = document.querySelector('.menu-list');
 const $modalContainer = document.querySelector('.menu-modal-container');
-
+// const $btnNext = document.querySelector('.btn-next');
+// const $btnPrev = document.querySelector('.btn-prev');
 // Function
 const getMenu = async (url, callback) => {
   try {
     const menu = await model.getMenu(url);
+    //const pageMenu = menu.slice(start, end);
     callback(menu);
   } catch (e) {
     console.error(e);
@@ -22,6 +27,18 @@ $menuList.addEventListener('click', (e) => {
   $modalContainer.style.display = 'flex';
   getMenu(`/${model.state}/${e.target.id}`, menuModalRender); //
 });
+
+// $btnNext.addEventListener('click', (e) => {
+//   start = end;
+//   end += end;
+//   getMenu(`/${model.state}`, menuRender);
+//   $btnPrev.style.display = 'block';
+// });
+// $btnPrev.addEventListener('click', (e) => {
+//   end = end - start;
+//   start = end - 8;
+//   getMenu(`/${model.state}`, menuRender);
+// });
 
 document.addEventListener('DOMContentLoaded', () => {
   getMenu(`/${model.state}`, menuRender); //
